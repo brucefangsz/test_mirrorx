12359568ecb0e9e82b4971bb17937e3e33cc002c
shuaizhong <fangshzho@yonyou.com>
2018-02-01 13:46:03 +0800

    up mirrorx get list

--- src/components/Welcome/wecomemodel.jsx
+++ src/components/Welcome/wecomemodel.jsx
@@ -1,4 +1,5 @@
 import { actions } from "mirrorx";
+import axios from "axios";
 export default {
   name: "fsztest",
   initialState: {
@@ -14,5 +15,17 @@ export default {
       return { ...state, ...data };
     }
   },
-  effects: {}
+  effects: {
+    getList() {
+      actions.fsztest.save({ user: { name: "房宏炎", age: 24, sex: 0 } });
+    },
+    async getUserList() {
+      let { data: { data: list } } = await axios.get(
+        "https://mock.yonyoucloud.com/mock/56/nc/api/user"
+      );
+      actions.fsztest.save({ list });
+      console.log("获取线上数据");
+      console.log(list);
+    }
+  }
 };
--- src/components/Welcome/welcomecontainal.jsx
+++ src/components/Welcome/welcomecontainal.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
-
+import { actions } from "mirrorx";
 const Welcome = state => {
-  console.log(state);
-  return <div>hhh</div>;
+  return (
+    <div>
+      <button onClick={actions.fsztest.getList}>点击我吧</button>
+      <br />
+      <div>{`姓名：${state.user.name}`}</div>
+      <div>{`年龄：${state.user.age}`}</div>
+      <div>{`性别：${state.user.sex}`}</div>
+      <button onClick={actions.fsztest.getUserList}>获取线上数据</button>
+      {state.list.map(item => {
+        return [
+          <div>{item.username}</div>,
+          <div>{item.age}</div>, 
+          <div>{item.sex}</div>
+        ];
+      })}
+    </div>
+  );
 };
 export default Welcome;
